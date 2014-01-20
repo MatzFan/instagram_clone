ffdb22351e148b904b3eae287b750f1f03f7ab3f
Bruce Steedman <bruce.steedman@gmail.com>
2014-01-20 10:55:32 +0000

    Setup new Rails app - initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InstagramClone::Application.config.secret_key_base = '903586b80b4ca8a05fb040173dadc2a1e352997d28142dbce0674a9b19cde1188a93637962ab357f3d2179ecfcc53793637de9b490304d8a0a6046802f620a78'
