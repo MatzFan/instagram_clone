2c61e9c9a64998517d0a6563f8d8b0c3145073ae
Bruce Steedman <bruce.steedman@gmail.com>
2014-01-20 21:32:17 +0000

    Deleted old migration

--- db/migrate/20140120185122_add_user_id_to_posts.rb
+++
@@ -1,8 +0,0 @@
-class AddUserIdToPosts < ActiveRecord::Migration
-  def change
-    # this adds user ref to posts - i.e. post is child of user
-    add_reference :posts, :user, index: true
-    # foreigner gem allows FK's t be created like so:-
-    add_foreign_key(:user, :posts)
-  end
-end
--- db/migrate/20140120212212_add_user_id_to_posts.rb
+++ db/migrate/20140120212212_add_user_id_to_posts.rb
@@ -1,6 +1,5 @@
 class AddUserIdToPosts < ActiveRecord::Migration
   def change
     add_foreign_key(:posts, :users)
-    # add_reference :posts, :user, index: true
   end
 end
